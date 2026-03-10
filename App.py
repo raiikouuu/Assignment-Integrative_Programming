a8af31e27ce62a948253021dc66ed8c162d33f5e
Sardaña,John Raymond S. <johnraysardana24@gmail.com>
2026-03-10 17:19:10 +0800

    Refactor Pokémon app layout and type icons

--- App.py
+++ App.py
@@ -3,7 +3,12 @@ import requests
 import random
 import pandas as pd
 
-st.set_page_config(page_title="Pokédex App", page_icon="⚪")
+POKEBALL_ICON = "https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg"
+st.set_page_config(
+    page_title="Pokédex App",
+    page_icon=POKEBALL_ICON,
+    layout="centered"
+)
 
 BACKGROUND = """
 <style>
@@ -23,22 +28,22 @@ text-shadow:2px 2px 4px black;
 """
 st.markdown(BACKGROUND, unsafe_allow_html=True)
 
-type_sprites = {
-"Normal":"https://archives.bulbagarden.net/media/upload/8/83/Normal_icon_SwSh.png",
-"Fire":"https://archives.bulbagarden.net/media/upload/a/ab/Fire_icon_SwSh.png",
-"Water":"https://archives.bulbagarden.net/media/upload/8/80/Water_icon_SwSh.png",
-"Electric":"https://archives.bulbagarden.net/media/upload/7/7b/Electric_icon_SwSh.png",
-"Grass":"https://archives.bulbagarden.net/media/upload/a/a8/Grass_icon_SwSh.png",
-"Ice":"https://archives.bulbagarden.net/media/upload/1/15/Ice_icon_SwSh.png",
-"Fighting":"https://archives.bulbagarden.net/media/upload/7/7d/Fighting_icon_SwSh.png",
-"Poison":"https://archives.bulbagarden.net/media/upload/8/8d/Poison_icon_SwSh.png",
-"Ground":"https://archives.bulbagarden.net/media/upload/8/8f/Ground_icon_SwSh.png",
-"Flying":"https://archives.bulbagarden.net/media/upload/b/b5/Flying_icon_SwSh.png",
-"Psychic":"https://archives.bulbagarden.net/media/upload/7/73/Psychic_icon_SwSh.png",
-"Bug":"https://archives.bulbagarden.net/media/upload/9/9c/Bug_icon_SwSh.png",
-"Rock":"https://archives.bulbagarden.net/media/upload/b/bb/Rock_icon_SwSh.png",
-"Ghost":"https://archives.bulbagarden.net/media/upload/0/01/Ghost_icon_SwSh.png",
-"Dragon":"https://archives.bulbagarden.net/media/upload/0/03/Dragon_icon_SwSh.png"
+type_icons = {
+"Normal":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/normal.svg",
+"Fire":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/fire.svg",
+"Water":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/water.svg",
+"Electric":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/electric.svg",
+"Grass":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/grass.svg",
+"Ice":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/ice.svg",
+"Fighting":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/fighting.svg",
+"Poison":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/poison.svg",
+"Ground":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/ground.svg",
+"Flying":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/flying.svg",
+"Psychic":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/psychic.svg",
+"Bug":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/bug.svg",
+"Rock":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/rock.svg",
+"Ghost":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/ghost.svg",
+"Dragon":"https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/dragon.svg"
 }
 
 @st.cache_data
@@ -47,41 +52,17 @@ def get_gen1_pokemon():
     data = requests.get(url).json()
     return [p["name"].capitalize() for p in data["results"]]
 
-
 def get_pokemon_info(name):
-
-    data = requests.get(
-        f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
-    ).json()
-
+    data = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name.lower()}").json()
     sprite = data["sprites"]["front_default"]
-
     types = [t["type"]["name"].capitalize() for t in data["types"]]
-
-    stats = {
-        s["stat"]["name"].capitalize(): s["base_stat"]
-        for s in data["stats"]
-    }
-
+    stats = {s["stat"]["name"].capitalize(): s["base_stat"] for s in data["stats"]}
     return sprite, types, stats
 
-
 def get_type_matchup(type_name):
-
-    data = requests.get(
-        f"https://pokeapi.co/api/v2/type/{type_name.lower()}"
-    ).json()
-
-    strengths = [
-        t["name"].capitalize()
-        for t in data["damage_relations"]["double_damage_to"]
-    ]
-
-    weaknesses = [
-        t["name"].capitalize()
-        for t in data["damage_relations"]["double_damage_from"]
-    ]
-
+    data = requests.get(f"https://pokeapi.co/api/v2/type/{type_name.lower()}").json()
+    strengths = [t["name"].capitalize() for t in data["damage_relations"]["double_damage_to"]]
+    weaknesses = [t["name"].capitalize() for t in data["damage_relations"]["double_damage_from"]]
     return strengths, weaknesses
 
 pokemon_list = get_gen1_pokemon()
@@ -94,23 +75,15 @@ if page == "Home":
 
     trainer = st.text_input("Trainer Name")
 
-    level = st.slider("Trainer Level",1,100,10)
+    level = st.slider("Trainer Level", 1, 100, 10)
 
-    starter = st.radio(
-        "Choose Starter Pokémon",
-        ["Bulbasaur","Charmander","Squirtle"]
-    )
+    starter = st.radio("Choose Starter Pokémon", ["Bulbasaur", "Charmander", "Squirtle"])
 
     if st.button("Start Adventure"):
-
         sprite, types, stats = get_pokemon_info(starter)
-
         st.success(f"Trainer {trainer} chose {starter}")
-
-        st.image(sprite,width=200)
-
-        st.write("Type:",", ".join(types))
-
+        st.image(sprite, width=200)
+        st.write("Type:", ", ".join(types))
         st.balloons()
 
     st.divider()
@@ -119,21 +92,25 @@ if page == "Home":
 
     cols = st.columns(5)
 
-    for i, type_name in enumerate(type_sprites):
-
+    for i, type_name in enumerate(type_icons):
         with cols[i % 5]:
-
-            st.image(type_sprites[type_name],width=60)
-
+            st.image(type_icons[type_name], width=50)
             if st.button(type_name):
-
                 strengths, weaknesses = get_type_matchup(type_name)
-
-                st.subheader(type_name)
-
+                st.write("###", type_name)
                 st.write("⚔️ Strong Against:", ", ".join(strengths) if strengths else "None")
-
                 st.write("⚠️ Weak Against:", ", ".join(weaknesses) if weaknesses else "None")
+
+    st.divider()
+
+    st.subheader("🎲 Discover a Random Pokémon")
+
+    if st.button("Generate Random Pokémon"):
+        random_pokemon = random.choice(pokemon_list)
+        sprite, types, stats = get_pokemon_info(random_pokemon)
+        st.write(f"You discovered **{random_pokemon}!**")
+        st.image(sprite, width=200)
+        st.write("Type:", ", ".join(types))
 
 elif page == "Pokédex":
 
@@ -143,19 +120,11 @@ elif page == "Pokédex":
 
     sprite, types, stats = get_pokemon_info(selected)
 
-    st.image(sprite,width=200)
-
+    st.image(sprite, width=200)
     st.subheader(selected)
-
-    st.write("Type:",", ".join(types))
+    st.write("Type:", ", ".join(types))
 
     st.divider()
-
     st.subheader("📊 Base Stats")
-
-    stats_df = pd.DataFrame(
-        list(stats.items()),
-        columns=["Stat","Base Value"]
-    )
-
+    stats_df = pd.DataFrame(list(stats.items()), columns=["Stat", "Base Value"])
     st.table(stats_df)
